63405d045442170203d24b835208a6134bdc38dd
Franklin Esquivel <franklin@ese.plus>
2020-09-30 22:39:09 -0600

    refactor: Remove bot functionality

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,4 @@ server.listen(__port__, () =>
 	console.log(`Server stated! Listening at http://localhost:${__port__}`)
 );
 
-bot.login(__disc_token__ ?? undefined).catch(err => console.log(err)).then(() => console.log('Bot up!'));
+// bot.login(__disc_token__ ?? undefined).catch(err => console.log(err)).then(() => console.log('Bot up!'));
