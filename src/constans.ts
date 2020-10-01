7c40a875d49f727c32df4423fb2091b9e9c20dc1
Franklin Esquivel <franklin@ese.plus>
2020-09-30 22:16:32 -0600

    fix: Server port

--- src/constans.ts
+++ src/constans.ts
@@ -3,6 +3,6 @@
  *  - Tokens, ports, API URL's, etc.
  */
 
-export const __port__ = process.env.PORT || 5000;
+export const __port__ = process.env.PORT ?? 5000;
 export const __prod__ = process.env.NODE_ENV === 'prod';
 export const __disc_token__ = process.env.DISCORD_TOKEN ?? null;
